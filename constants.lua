8a376c40b2c8e65d4617db2984b1bfde7e4eb564
Jesse Ruder <jesse@getexponent.com>
2019-07-08 16:24:50 -0700

    basic 3d working

---
+++ constants.lua
@@ -0,0 +1,9 @@
+-- for capping game logic at 60 manually
+LogicRate = 60
+LogicAccumulator = 0
+PhysicsStep = true
+
+FogColor = {1,1,1,1}
+FogStartDist = 0
+FogDivide = 10
+FontColor = {0.3, 0.3, 0.3, 1}
